543ba1b54d606efd0764ff12717ef489df5b31a0
Suraj Kumar Behera <55729457+SurajBehera93@users.noreply.github.com>
2023-07-03 11:56:48 +0530

    Done

---
+++ demo_python.py
@@ -0,0 +1,21 @@
+# List
+suraj = ["vicky", "micky", 20.5, True, 9500]
+print(suraj[-1])
+suraj.insert(3, "tina")
+print(suraj)
+suraj.append(500)
+suraj[1] = "sonali"
+print(suraj)
+suraj.pop(0)
+print(suraj)
+
+# dictionary
+dic = {1: "Suraj", "bc": 500}
+print(dic["bc"])
+vic = (500, "man", True, 250.55)
+print(vic[1])
+dict["b"] = 5000
+
+dict = {}
+dict[1] = "Sanatan"
+print(dict)
---
+++ functionDemo.py
@@ -0,0 +1,5 @@
+def sumNumbers(a, b, c):
+    print(a + b + c)
+
+
+sumNumbers(10, 10, 10)
