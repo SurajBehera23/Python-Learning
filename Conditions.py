8ec53856718473dbc4383b92b403268464389a92
Suraj Kumar Behera <55729457+SurajBehera93@users.noreply.github.com>
2023-07-03 19:02:41 +0530

    today

--- Conditions.py
+++ Conditions.py
@@ -10,7 +10,7 @@ for i in suraj:
     print(i * 2)
 print("****************")
 
-suma = 1
+suma = 0
 for k in range(1, 6):
     suma = suma + k
 print(suma)
---
+++ py_input.py
@@ -0,0 +1,3 @@
+name = eval(input("Input text"))
+print(name)
+print(type(name))
