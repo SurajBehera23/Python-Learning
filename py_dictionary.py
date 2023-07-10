6138809a94788774c187d3e2fd6f1a1e0831e48b
Suraj Kumar Behera <55729457+SurajBehera93@users.noreply.github.com>
2023-07-10 15:31:34 +0530

    test

---
+++ py_dictionary.py
@@ -0,0 +1,18 @@
+name = {"First name": "Suraj", "Last name": "Behera", "Middle Name": "Kumar"}
+print(type(name))
+'''print(name)
+print(name["First name"])
+
+# pop method
+name.pop("Middle Name")  # Corrected key name
+print(name)
+
+# Get method
+print(name.get("Last name"))  # Added print statement
+
+# keys method
+print(name.keys())
+'''
+# for loop
+for every, values in name.items():
+    print(every, values,sep="-")#Added separator
