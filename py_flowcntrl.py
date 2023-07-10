568b1d233de0b284e76bae3a2291321977d35a80
Suraj Kumar Behera <55729457+SurajBehera93@users.noreply.github.com>
2023-07-10 18:37:09 +0530

    login flow control

---
+++ py_flowcntrl.py
@@ -0,0 +1,25 @@
+user_input = int(input("Enter a number - "))
+if user_input < 100:
+    print("less than 100")
+
+elif user_input == 100:
+    print("Value is equal to 100")
+else:
+    print("Value is greater than 100")
+
+
+#login
+input_password = "8093479088"
+input_UserName = "Vicky"
+
+my_inputUser = input("Enter UserName - ")
+my_inputPass = input("Enter Password - ")
+
+if my_inputUser != input_UserName and my_inputPass != input_password:
+    print("Invalid username and password")
+elif my_inputUser != input_UserName:
+    print("Invalid username")
+elif my_inputPass != input_password:
+    print("Invalid password")
+else:
+    print("Login Success")
