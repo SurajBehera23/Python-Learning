3a5c14da500bb0a67296a4cbaad122bc4e725399
Suraj Kumar Behera <55729457+SurajBehera93@users.noreply.github.com>
2023-07-18 14:14:19 +0530

    learnt fileio and oop

---
+++ py_fileIO.py
@@ -0,0 +1,32 @@
+
+#
+# try:
+#     mytxt = open("C:\\Users\\NBDPLN0021\\Desktop\\python\\vky.txt", "r")
+#     print(mytxt.readlines())
+#
+# except:
+#     print("File not available")
+# else:
+#     mytxt.close()
+#     print("file closed")
+#
+#file copy
+# try:
+#     with open("C:\\Users\\NBDPLN0021\\Desktop\\python\\vky.txt") as a:
+#         with open("C:\\Users\\NBDPLN0021\\Desktop\\python\\vky2.txt", "w") as b:
+#             for i in a:
+#                 b.write(i)
+#
+# except:
+#     print("File not available")
+# else:
+#     a.close()
+#     print("file closed")
+
+#file delete
+import os
+if os.path.exists("C:\\Users\\NBDPLN0021\\Desktop\\python\\vkyh.txt"):
+    os.remove("C:\\Users\\NBDPLN0021\\Desktop\\python\\vky.txt")
+    print("File deleted")
+else:
+    print("File not avaialable") 
---
+++ py_oop.py
@@ -0,0 +1,8 @@
+class A:
+    midage = int(input("enter mid age"))
+    topage = int(input("enter top age"))
+    total = midage + topage
+    print(total)
+
+
+obj = A()
